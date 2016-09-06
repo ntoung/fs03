6f96ad47f33a87660eb3b8e49676244c9bdfb3fc
Nathan <ntoung@Nathans-MacBook-Air.local>
2016-09-05 22:12:18 -0700

    fibonacci

--- intermediate.js
+++ intermediate.js
@@ -96,5 +96,28 @@ function primeFactors(n) {
 }
 
 
+/* 4. Fibonacci */
+/* How do you get the nth Fibonacci number? */
+
+// Method 1 (simple)
+function fibonacci(n) {
+	var fibonacci = [0, 1];
+	if (n <= 2) return 1;
+
+	for (var i = 2; i <= n; i++) {
+		fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+	}
+
+	return fibonacci[n];
+}
+
+// Method 2 (recursive)
+function fibonacci(n) {
+	if (n <= 1) 
+		return n;
+	else 
+		return fibonacci(n-1) + fibonacci(n-2);
+}
+
 
 
